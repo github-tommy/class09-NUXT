0f8cf7a8d78d08dde52dff792e13bc999155b13b
tom <tommy.surjadi@gmail.com>
2019-06-03 23:03:03 -0700

    add li

--- pages/next.vue
+++ pages/next.vue
@@ -3,32 +3,58 @@
   <div class="box">
   <h1>{{message1}}</h1>
   <br/>
+  <h2>Input the city you live now</h2>
+   <br/>
+  <input v-model="message2"/>
+   <br/>
   <h2>{{message2}}</h2>
   <br/>
-  <input v-model="message2"/>
+  <h2>Why Washington?</h2>
   <br/>
-  <br/>
-  <button><a v-bind:href="url">Click me for Joke</a></button>
-  
+    <ul>
+      <li v-for="reason in reasons">
+        {{ reason.name }}
+      </li>
+    </ul>
+    </br>
+    <button v-on:click="pressed">Press Me</button>
+    <br/>
+    <br/>
+    <button><a v-bind:href="url">Click Me for Joke</a></button>
 </div>
-
  </div>
 </template>
 
 <script>
-
-
 import axios from 'axios'
 
 export default {
  data() {
    return {
-     message1: 'hello world from tommy',
-     message2: '=> rewrite something on the box below',
+     message1: '10 Reason to Invest in Seattle WA - by tommys',
+     message2: '',
+     reasons: [
+       {name: 'Economically robust and expanding'},
+       {name: 'Pivotal west coast and asia pacific location'},
+       {name: 'A new emerging tech hub'},
+       {name: 'Economically robust and expanding'},
+       {name: 'Favorable tax environment'},
+       {name: 'A competitively priced globally'},
+       {name: 'Pivotal west coast and asia pacific location'},
+       {name: 'World-leading education'},
+       {name: 'Ethically diverse'},
+       {name: 'A new emerging tech hub'}
+     ],
      url: 'https://icanhazdadjoke.com'
    }
+   },  //return
+
+ methods: {
+   pressed(){
+     alert('Thanks for Pressing')
+   }
  }
-}
+} //default
 
 </script>
 
